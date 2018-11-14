1e557e24deae30ff141b47317b677c8888bfbee6
Sahra Vadrot <apexp@hotmail.fr>
2018-11-14 11:45:10 +0100

    add materialize to the project

--- django1/settings.py
+++ django1/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django1.apps.Django1Config',
     'elementaryCellularAutomata.apps.ElementarycellularautomataConfig',
     'pasteAsMarkdown.apps.PasteasmarkdownConfig',
     'django.contrib.admin',
--- django1/urls.py
+++ django1/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
+app_name = "django1"
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path('admin/', admin.site.urls),
     path('elementaryCellularAutomata/', include('elementaryCellularAutomata.urls')),
     path('pasteAsMarkdown/', include('pasteAsMarkdown.urls'))
